5daed42c4e949c2ece606df919c286335ec3708d
harshalpatil <harshalpatil.er@gmail.com>
2021-08-29 11:45:20 +0530

    Example of event

--- PipesDemo/src/app/app.component.ts
+++ PipesDemo/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PipesDemo';
+  clickfun() {
+    console.log("Simple example of Click event");
+  }
+  clickfun2() {
+    console.log("This is written in conical form of event");
+  }
+  keyUp() {
+    console.log('This is keyup event')
+  }
+  keyDown() {
+    console.log('This is key down event'
+    )
+  }
+  input(){
+    console.log('This is input event');
+  }
 }
